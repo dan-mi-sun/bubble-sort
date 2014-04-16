8ab56c2235bca0b6f83571a1a29b2857adf2b0b9
Daniel Sun <dan.mi.sun@gmail.com>
2014-04-16 17:24:58 +0100

    Upto stage 5 of 15 - first loop with sorting

---
+++ create_array.rb
@@ -0,0 +1,3 @@
+class Array
+  array = [8, 9, 2, 3, 4, 5, 6, 6]
+end
--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -5,18 +5,35 @@ class Array
   # numeric)
   #
   def sum
-    # puts self.join(",")
+ #    puts self.join(",")
   end
-
   # Returns the prime numbers in this array
   def primes
-    # puts self.join(",")
+ #    puts self.join(",")
   end
 
   # Examine each pair in myself and swap them until all my
   # elements are in order
-  #
   def bubble_sort
-  	# puts self.join(",")
+  #puts self.join(",")
+    i = 0
+    m = 0
+    n = 1
+    while i < (self.length - 1)
+      stack = []
+      pair = self[i,2]
+      if pair[0] > pair[1]
+        stack.push pair[0]
+        stack.push pair[1]
+        self[m] = stack.pop
+        self[n] = stack.pop
+      end
+      m += 1
+      n += 1
+      i += 1
+      print self.join(",")
+
+      binding.pry
+    end
   end
 end
